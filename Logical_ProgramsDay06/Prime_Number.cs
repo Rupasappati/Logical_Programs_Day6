4137f3a1f1f88a15b3d748a61019deac5316050c
Rupasappati <rupasappati@gmail.com>
2022-05-10 17:38:01 +0530

    Added: Prime number Functionality

---
+++ Logical_ProgramsDay06/Prime_Number.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_ProgramsDay06
+{
+    public class Prime_Number
+    {
+    public void PrimeNum()
+        {
+            int num;
+            int x=0;
+            Console.Write("\n");
+            Console.WriteLine("***Display the Prime Numbers***");
+            Console.Write("\n");
+
+            Console.WriteLine("Please Enter a number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 2; i < num/2; i++)
+            {
+                if(num % i == 0)
+                {
+                    x++;
+                       
+                }
+            }
+            if(x==0 && num != 0)
+            {
+                Console.WriteLine("{0} is Prime Number", num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a Prime Number", num);
+            }
+            Console.WriteLine("\n");
+        }
+
+    }
+}
--- Logical_ProgramsDay06/Program.cs
+++ Logical_ProgramsDay06/Program.cs
@@ -13,6 +13,7 @@ namespace Logical_ProgramsDay06
                 Console.WriteLine("Which program would you like to excute ?");
                 Console.WriteLine("Q1.Fibonacci Series");
                 Console.WriteLine("Q2.Perfect Number");
+                Console.WriteLine("Q3.Prime Number");
                 Console.WriteLine("Please enter the Question Number what you want to excute");
                 int userChoice = Convert.ToInt32(Console.ReadLine());
                 switch (userChoice)
@@ -26,6 +27,11 @@ namespace Logical_ProgramsDay06
                         Console.WriteLine("You have choosen Q2");
                         Perfect_Number number = new Perfect_Number();
                         number.perfectNum();                      
+                        break;
+                    case 3:
+                        Console.WriteLine("You have choosen Q3");
+                        Prime_Number prime_Number = new Prime_Number();
+                        prime_Number.PrimeNum();
                         break;
                         default:
                         Console.WriteLine("Ivalid Input");
