d511f19a41a19f17ad2dba5a82ab41a7942729c3
Rupasappati <rupasappati@gmail.com>
2022-05-10 14:22:49 +0530

    Added: Fibonacci Series

---
+++ Logical_ProgramsDay06/Fibonacci_Series.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs_Day6
+{
+    public class Fibonacci_Series
+    {
+        public void FibonacciSeries()
+        {
+            int num, i;
+            int present = 1;
+            int previous = 0;
+            int sum;
+            Console.WriteLine("***Display the Fibonacci Series***");
+            Console.Write("\n\n\n");
+
+            //User Input
+            Console.Write("Please Enter the Fibonacci number :");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Fibonacci series of the enterd Number is {0} : ", num);
+            Console.Write(" {0} {1}", previous, present);
+
+            for (i = 2; i < num; i++)
+            {
+                sum = previous + present;
+                Console.Write(" {0} ", sum);
+                previous = present;
+                present = sum;
+
+            }
+            Console.Write("\n");
+        }
+
+    }
+}
---
+++ Logical_ProgramsDay06/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Logical_Programs_Day6
+{
+    public class Program
+    {
+        static void Main(String[] args)
+
+        {
+            bool option = true;
+            while (option == true)
+            {
+                Console.WriteLine("Which program would you like to excute ?");
+                Console.WriteLine("Q1.Fibonacci Series");
+                Console.WriteLine("Please enter the Question Number what you want to excute");
+                int userChoice = Convert.ToInt32(Console.ReadLine());
+                switch (userChoice)
+                {
+                    case 1:
+                        Console.WriteLine("You have choosen Q1");
+                        Fibonacci_Series series = new Fibonacci_Series();
+                        series.FibonacciSeries();
+                        break;
+                }
+            }
+
+        }
+    }
+}
+
+
+
